cd49684d94eabe17c9a440110f5b456ca37855cc
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-14 20:48:19 +0000

    sdsd

--- src/components/Child.js
+++ src/components/Child.js
@@ -3,7 +3,6 @@ import React,{useState} from 'react'
 let Child = (props)=>{
     return(
         <div className='child'>
-            <p>Child Component {props.key}</p>
             <button onClick={()=>{props.set('Option '+props.k)}}>{'Option '+props.k}</button>
         </div>
     )
